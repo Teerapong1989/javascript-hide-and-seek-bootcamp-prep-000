35737a38ff7c0a0f5a1232800f13e95d281adc49
Teerapong Dinon <tp.dinon@gmail.com>
2019-02-08 01:14:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-function getFirstSelector(selector){
-  return document.querySelector(selector)
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
 }
 
-function nestedTarget(){
-  return document.querySelector('#nested .target')
+function nestedTarget() {
+  return document.querySelector('#nested .target');
 }
 
-function increaseRankBy(n){
+function increaseRankBy(n) {
   var ranks = document.querySelectorAll('ul.ranked-list li');
   for (var i = 0; i < ranks.length; i++) {
     ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
-  } 
+  }
 }
 
-function deepestChild(){
+function deepestChild() {
   var grandNode = document.querySelector('#grand-node');
   var deepestNode = grandNode.children[0];
   for (var i = 0; !deepestNode.children[i]; i) {
