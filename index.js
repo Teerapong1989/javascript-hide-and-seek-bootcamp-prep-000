246398bb164908c6800438bc49a6a3eb2b45c13a
Teerapong Dinon <tp.dinon@gmail.com>
2019-02-08 00:59:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function nestedTarget(){
 function increaseRankBy(n){
   var rank = document.querySelectorAll('ul.ranked-list li')
   for(let i = 0; i < lis.lenght; i++){
-    rank[i].innerHTML = (i + 1).ranked-list(n);
+    rank[i].innerHTML = parseInt()
   }
 }
